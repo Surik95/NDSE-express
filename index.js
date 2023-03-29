603be03497ced6b631be653708e924961d699fe1
Surik95 <pokemon.surik@gmail.com>
2023-03-29 15:34:20 +0300

    выполнил задание 2.2. Middleware

--- index.js
+++ index.js
@@ -1,112 +1,16 @@
 import express from 'express';
-import { nanoid } from 'nanoid';
-
-class Book {
-  constructor(
-    title = 'string',
-    description = 'string',
-    authors = 'string',
-    favorite = 'string',
-    fileCover = 'string',
-    fileName = 'string',
-    id = nanoid()
-  ) {
-    this.id = id;
-    this.title = title;
-    this.description = description;
-    this.authors = authors;
-    this.favorite = favorite;
-    this.fileCover = fileCover;
-    this.fileName = fileName;
-    this.title = title;
-    this.id = id;
-  }
-}
-
-const stor = {
-  books: [],
-};
+import login from './routes/login.js';
+import book from './routes/book.js';
+import index from './routes/index.js';
+import downloadImg from './routes/downloadImg.js';
 
 const app = express();
 app.use(express.json());
 
-app.post('/api/user/login', (req, res) => {
-  res.status(201);
-  res.json({ id: 1, mail: 'test@mail.ru' });
-});
-
-app.get('/api/books', (req, res) => {
-  const { books } = stor;
-  res.json(books);
-});
-
-app.get('/api/books/:id', (req, res) => {
-  const { books } = stor;
-  const { id } = req.params;
-  const idx = books.findIndex((el) => el.id === id);
-
-  if (idx !== -1) {
-    res.json(books[idx]);
-  } else {
-    res.status(404);
-    res.json({ errcode: 404, errmsg: '“not found”' });
-  }
-});
-
-app.post('/api/books/', (req, res) => {
-  const { books } = stor;
-  const { title, description, authors, favorite, fileCover, fileName } =
-    req.body;
-  const newBook = new Book(
-    title,
-    description,
-    authors,
-    favorite,
-    fileCover,
-    fileName
-  );
-  books.push(newBook);
-
-  res.status(201);
-  res.json(newBook);
-});
-
-app.put('/api/books/:id', (req, res) => {
-  const { books } = stor;
-  const changes = {};
-  for (const key in req.body) {
-    if (req.body[key]) {
-      changes[key] = req.body[key];
-    }
-  }
-  const { id } = req.params;
-  const idx = books.findIndex((el) => el.id === id);
-  if (idx !== -1) {
-    books[idx] = {
-      ...books[idx],
-      ...changes,
-    };
-
-    res.json(books[idx]);
-  } else {
-    res.status(404);
-    res.json({ errcode: 404, errmsg: '“not found”' });
-  }
-});
-
-app.delete('/api/books/:id', (req, res) => {
-  const { books } = stor;
-  const { id } = req.params;
-  const idx = books.findIndex((el) => el.id === id);
-
-  if (idx !== -1) {
-    books.splice(idx, 1);
-    res.json(true);
-  } else {
-    res.status(404);
-    res.json({ errcode: 404, errmsg: '“not found”' });
-  }
-});
+app.use('/api/user/login', login);
+app.use('/api/books/', index);
+// app.use('/api/books/:id/download', downloadImg);
+// app.use('/api/books/:id', book);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT);
